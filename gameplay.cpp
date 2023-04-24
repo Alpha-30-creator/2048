1f6440d241a8f1aa5acb48989e37566d5894d764
Muhammad Abdur Rahman Saad <63783742+Alpha-30-creator@users.noreply.github.com>
2023-04-24 14:00:15 +0800

    Update gameplay.cpp
    minor tweaks to calculateScore and initializeBoard functions

--- gameplay.cpp
+++ gameplay.cpp
@@ -19,7 +19,7 @@ void initializeBoard(int size, vector<vector<int>> &board) {
     srand(time(0));
 
     // Place two 2s at random positions on the board
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 2; i++) {
         int x, y;
         // Generate random positions until an empty position is found
         while (true) {
@@ -59,19 +59,15 @@ void calculateScore(char move, int &score, int size, vector<vector<int>> &board)
     }
     // calculate score if move is right
     else if (move == 'd') {
-        // search through board row by row from right to left
+        // same steps for score calculation but search through board row by row from right to left
         for (int i = size - 1; i > -1; i--) {
-            //reset temp to 0 for each row
             temp = 0;
             for (int j = 0; j < size; j++) {
-                // if current element of board is not equal to the previous element, previous element becomes the current element
                 if (board[i][j] != temp && board[i][j] != 0){
                     temp = board[i][j];
                 }
-                //if previous element equal to current element, add their sum to score
                 else if (board[i][j] == temp){
                     score += board[i][j] * 2;
-                    // set temp(previous element) to 0, so that no odd number of adjacent, similar tiles are added to score
                     temp = 0;
                 }
             }
@@ -79,19 +75,15 @@ void calculateScore(char move, int &score, int size, vector<vector<int>> &board)
     }
     //calculate score if move is up
     else if (move == 'w') {
-        // search through board column by column from top to bottom
+        // same steps for score calculation but search through board column by column from top to bottom
         for (int i = 0; i < size; i++) {
-            //reset temp to 0 for each column
             temp = 0;
             for (int j = 0; j < size; j++) {
-                // if current element of board is not equal to the previous element, previous element becomes the current element
                 if (board[j][i] != temp && board[j][i] != 0){
                     temp = board[j][i];
                 }
-                //if previous element equal to current element, add their sum to score
                 else if (board[j][i] == temp){
                     score += board[j][i] * 2;
-                    // set temp(previous element) to 0, so that no odd number of adjacent, similar tiles are added to score
                     temp = 0;
                 }
             }
@@ -99,19 +91,15 @@ void calculateScore(char move, int &score, int size, vector<vector<int>> &board)
     }
     //calculate score if move is down
     else if (move == 's') {
-        // search through board column by column from bottom to top
+        // same steps for score calculation but search through board column by column from bottom to top
         for (int i = 0; i < size; i++) {
-            //reset temp to 0 for each column
             temp = 0;
             for (int j = size - 1; j > - 1; j--) {
-                // if current element of board is not equal to the previous element, previous element becomes the current element
                 if (board[j][i] != temp && board[j][i] != 0){
                     temp = board[j][i];
                 }
-                //if previous element equal to current element, add their sum to score
                 else if (board[j][i] == temp){
                     score += board[j][i] * 2;
-                    // set temp(previous element) to 0, so that no odd number of adjacent, similar tiles are added to score
                     temp = 0;
                 }
             }

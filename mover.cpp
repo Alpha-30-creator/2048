67070639757393d2b75618f0bf5f2c883b88ec43
Mussabek Yerdaulet <53343530+myy04@users.noreply.github.com>
2023-04-26 23:19:41 +0800

    Add files via upload

---
+++ mover.cpp
@@ -0,0 +1,52 @@
+#include "mover.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void mover_update(auto a, int n, int x, int y, int dx, int dy) {
+	int new_x = x + dx, new_y = y + dy;
+	if (new_x < 1 || new_y < 1 || new_x > n || new_y > n) return;
+
+	if (a[x][y] == a[new_x][new_y]) {
+		a[x][y] *= 2;
+		a[new_x][new_y] = 0;
+	}
+	else if (a[x][y] == 0) {
+		a[x][y] = a[new_x][new_y];
+		a[new_x][new_y] = 0;
+	}
+
+	mover_update(a, n, new_x, new_y, dx, dy);
+}	
+
+
+void move(auto a, int n, string dir) {
+	for (char &i : dir) {
+		i = tolower(i);
+	}
+
+	if (dir == "left") {
+		for (int i = 1; i <= n; i++) {
+			mover_update(a, n, i, 1, 0, 1);
+		}
+	}
+	
+	if (dir == "right") {
+		for (int i = 1; i <= n; i++) {
+			mover_update(a, n, i, n, 0, -1);
+		}
+	}
+
+	if (dir == "up") {
+		for (int i = 1; i <= n; i++) {
+			mover_update(a, n, 1, i, 1, 0);
+		}
+	}
+
+	if (dir == "down") {
+		for (int i = 1; i <= n; i++) {
+			mover_update(a, n, n, i, -1, 0);
+		}
+	}
+}
---
+++ mover.h
@@ -0,0 +1,12 @@
+#ifndef MOVER_H
+#define MOVER_H
+
+#include <string>
+
+using namespace std;
+
+void update(auto a, int n, int x, int y, int dx, int dy);
+
+void move(auto a, int n, string dir);
+
+#endif

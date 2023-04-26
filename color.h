2d60f0071b2735b86c338fd08e012fa243b9596b
ShahJahanIshaq <shahjahanffc@gmail.com>
2023-04-27 04:40:01 +0800

    Make color.h cleaner

--- color.h
+++ color.h
@@ -4,42 +4,42 @@
 #include <ostream>
 
 namespace Color {
-enum class Code {
-  BOLD = 1,
-  RESET = 0,
-  BG_BLUE = 44,
-  BG_DEFAULT = 49,
-  BG_GREEN = 42,
-  BG_RED = 41,
-  FG_BLACK = 30,
-  FG_BLUE = 34,
-  FG_CYAN = 36,
-  FG_DARK_GRAY = 90,
-  FG_DEFAULT = 39,
-  FG_GREEN = 32,
-  FG_LIGHT_BLUE = 94,
-  FG_LIGHT_CYAN = 96,
-  FG_LIGHT_GRAY = 37,
-  FG_LIGHT_GREEN = 92,
-  FG_LIGHT_MAGENTA = 95,
-  FG_LIGHT_RED = 91,
-  FG_LIGHT_YELLOW = 93,
-  FG_MAGENTA = 35,
-  FG_RED = 31,
-  FG_WHITE = 97,
-  FG_YELLOW = 33,
-};
+    enum class Code {
+        BOLD = 1,
+        RESET = 0,
+        BG_BLUE = 44,
+        BG_DEFAULT = 49,
+        BG_GREEN = 42,
+        BG_RED = 41,
+        FG_BLACK = 30,
+        FG_BLUE = 34,
+        FG_CYAN = 36,
+        FG_DARK_GRAY = 90,
+        FG_DEFAULT = 39,
+        FG_GREEN = 32,
+        FG_LIGHT_BLUE = 94,
+        FG_LIGHT_CYAN = 96,
+        FG_LIGHT_GRAY = 37,
+        FG_LIGHT_GREEN = 92,
+        FG_LIGHT_MAGENTA = 95,
+        FG_LIGHT_RED = 91,
+        FG_LIGHT_YELLOW = 93,
+        FG_MAGENTA = 35,
+        FG_RED = 31,
+        FG_WHITE = 97,
+        FG_YELLOW = 33,
+    };
 
-class Modifier {
+    class Modifier {
 
-  Code code;
+        Code code;
 
-public:
-  Modifier(Code pCode) : code(pCode) {}
-  friend std::ostream &operator<<(std::ostream &os, const Modifier &mod) {
-    return os << "\033[" << static_cast<int>(mod.code) << "m";
-  }
-};
+        public:
+            Modifier(Code pCode) : code(pCode) {}
+            friend std::ostream &operator<<(std::ostream &os, const Modifier &mod) {
+                return os << "\033[" << static_cast<int>(mod.code) << "m";
+            }
+    };
 
 } // namespace Color
 

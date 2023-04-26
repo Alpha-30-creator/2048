c8f73c12e0fc9e982d0bcd9a919e32e139f34b67
Mussabek Yerdaulet <53343530+myy04@users.noreply.github.com>
2023-04-26 23:31:04 +0800

    Update mover.h

--- mover.h
+++ mover.h
@@ -6,7 +6,11 @@
 #include <vector>
 
 using namespace std;
+// function to move the board
+// a - board
+// n - size
+// dir - direction (up, down, left, right)
 
-void move(auto a, int n, string dir);
+void move(vector<vector<int>> &a, int n, string dir);
 
 #endif

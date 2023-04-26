f4d822559fb9dedbf85874753bb9a3b8f44a3304
Mussabek Yerdaulet <53343530+myy04@users.noreply.github.com>
2023-04-26 23:38:50 +0800

    Update mover.cpp

--- mover.cpp
+++ mover.cpp
@@ -2,49 +2,49 @@
 
 using namespace std;
 
-void mover_update(vector<vector<int>> &a, int n, int x, int y, int dx, int dy) {
+void mover_update(vector<vector<int>> &board, int size, int x, int y, int dx, int dy) {
 	int new_x = x + dx, new_y = y + dy;
-	if (new_x < 1 || new_y < 1 || new_x > n || new_y > n) return;
+	if (new_x < 1 || new_y < 1 || new_x > size || new_y > size) return;
 
-	if (a[x][y] == a[new_x][new_y]) {
-		a[x][y] *= 2;
-		a[new_x][new_y] = 0;
+	if (board[x][y] == board[new_x][new_y]) {
+		board[x][y] *= 2;
+		board[new_x][new_y] = 0;
 	}
-	else if (a[x][y] == 0) {
-		a[x][y] = a[new_x][new_y];
-		a[new_x][new_y] = 0;
+	else if (board[x][y] == 0) {
+		board[x][y] = board[new_x][new_y];
+		board[new_x][new_y] = 0;
 	}
 
-	mover_update(a, n, new_x, new_y, dx, dy);
+	mover_update(board, size, new_x, new_y, dx, dy);
 }	
 
-
-void move(vector<vector<int>> &a, int n, string dir) {
+//check header file for instructions
+void move(vector<vector<int>> &board, int size, string dir) {
 	for (char &i : dir) {
 		i = tolower(i);
 	}
 
 	if (dir == "left") {
-		for (int i = 1; i <= n; i++) {
-			mover_update(a, n, i, 1, 0, 1);
+		for (int i = 1; i <= size; i++) {
+			mover_update(board, size, i, 1, 0, 1);
 		}
 	}
 	
 	if (dir == "right") {
-		for (int i = 1; i <= n; i++) {
-			mover_update(a, n, i, n, 0, -1);
+		for (int i = 1; i <= size; i++) {
+			mover_update(board, size, i, size, 0, -1);
 		}
 	}
 
 	if (dir == "up") {
-		for (int i = 1; i <= n; i++) {
-			mover_update(a, n, 1, i, 1, 0);
+		for (int i = 1; i <= size; i++) {
+			mover_update(board, size, 1, i, 1, 0);
 		}
 	}
 
 	if (dir == "down") {
-		for (int i = 1; i <= n; i++) {
-			mover_update(a, n, n, i, -1, 0);
+		for (int i = 1; i <= size; i++) {
+			mover_update(board, size, size, i, -1, 0);
 		}
 	}
 }

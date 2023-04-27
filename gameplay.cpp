1ee06eadf7899abd22b3eb29aaa98cdb80dde607
Muhammad Abdur Rahman Saad <63783742+Alpha-30-creator@users.noreply.github.com>
2023-04-27 13:18:26 +0800

    Create gameplay.cpp
    update documentation

--- gameplay.cpp
+++ gameplay.cpp
@@ -6,9 +6,9 @@
 
 using namespace std;
 
-// Function to initialize the board takes size of the board and the board itself as input and returns the 2D board
+// Function to initialize the board 
 void initializeBoard(int size, vector<vector<int>> &board) {
-    // Resize the board with the given size and initialize its elements with 0 (empty positions)
+    // Resize the board with the given size and initialize its elements with 0(empty positions)
     for(int i= 0; i < size; i++){
         vector<int> row;
         for(int j = 0; j < size; j++){
@@ -39,27 +39,34 @@ void initializeBoard(int size, vector<vector<int>> &board) {
 //Function to calculate the score of the board after a move
 void calculateScore(char move, int &score, int size, vector<vector<int>> &board) {
     int temp = 0;
-    // calculate score if move is left
+    
+    // For score calculation, four directions need to considered: up, down, left and right.
+    
+    // *Explanation of logic*
+    // If move is in left direction, then we must loop through each element of the board from left to right(similar logic for rest of directions).
+    //temp variable will represent value of the previous tile.
+    //it will be set to 0 for each row
+    //if the current element of the board isn't equal to temp and the value of current element isn't 0. Tiles are not a match, temp set to current element.
+    //if temp is equal to current element, tiles are a match and we add their sum to the score
+    //similar steps for each direction with difference in the manner in which we loop through the board.
+    
+    //for left direction
     if (move == 'a') {
         // search through board row by row from left to right
         for (int i = 0; i < size; i++) {
-            //reset temp to 0 for each row
             temp = 0;
             for (int j = 0; j < size; j++) {
-                // if current element of board is not equal to the previous element, previous element becomes the current element
                 if (board[i][j] != temp && board[i][j] != 0){
                     temp = board[i][j];
                 }
-                //if previous element equal to current element, add their sum to score
                 else if (board[i][j] == temp){
                     score += board[i][j] * 2;
-                    // set temp(previous element) to 0, so that no odd number of adjacent, similar tiles are added to score
                     temp = 0;
                 }
             }
         }
     }
-    // calculate score if move is right
+    // for right direction
     else if (move == 'd') {
         // same steps for score calculation but search through board row by row from right to left
         for (int i = size - 1; i > -1; i--) {
@@ -75,7 +82,7 @@ void calculateScore(char move, int &score, int size, vector<vector<int>> &board)
             }
         }
     }
-    //calculate score if move is up
+    //for upward direction
     else if (move == 'w') {
         // same steps for score calculation but search through board column by column from top to bottom
         for (int i = 0; i < size; i++) {
@@ -91,7 +98,7 @@ void calculateScore(char move, int &score, int size, vector<vector<int>> &board)
             }
         }
     }
-    //calculate score if move is down
+    //for downward direction
     else if (move == 's') {
         // same steps for score calculation but search through board column by column from bottom to top
         for (int i = 0; i < size; i++) {

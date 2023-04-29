346ede8361e5b164d6152fec4bb12babd4d7861c
Zuhayr Abdullazhanov <115369065+loyaldept@users.noreply.github.com>
2023-04-30 05:30:27 +0800

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,9 +1,10 @@
+// This is a header file for the graphics module of the 2048 game.
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
 #include <vector>
 #include <string>
-
+// Function declarations for the graphics module
 void print_title();
 void print_red_divider();
 void print_instructions();

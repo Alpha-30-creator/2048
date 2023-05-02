71225a2f219c0ef373decef9ae5a229474197542
Muhammad Abdur Rahman Saad <63783742+Alpha-30-creator@users.noreply.github.com>
2023-05-03 00:38:30 +0800

    Create fileManipulation.h

---
+++ fileManipulation.h
@@ -0,0 +1,14 @@
+// fileManipulation.h
+#ifndef FILEMANIPULATION_H
+#define FILEMANIPULATION_H
+
+#include <string>;
+#include <vector>;
+using namespace std;
+
+void save_game(vector<vector<int>> board, int score, string username);
+vector<vector<int>> load_game(string username, int &score);
+vector<string> get_leaderboard();
+bool update_leaderboard(int score, string username);
+
+#endif

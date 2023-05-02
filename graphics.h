f058bd3e55f7d3dc64fbe6ea00d671a53bf535aa
Muhammad Abdur Rahman Saad <63783742+Alpha-30-creator@users.noreply.github.com>
2023-05-03 00:32:15 +0800

    Update graphics.h
    add all functions

--- graphics.h
+++ graphics.h
@@ -4,13 +4,18 @@
 
 #include <vector>
 #include <string>
+using namespace std;
+
 // Function declarations for the graphics module
+string get_chars_util(char ch, int number_of_chars);
 void print_title();
 void print_red_divider();
 void print_instructions();
 void display_loading();
-void display_board(std::vector<std::vector<int>>& board);
-void print_menu(std::string username);
+void display_board(svector<stdvector<int>>& board);
+void print_menu(string username);
+void print_difficulty_menu();
+void display_leaderboard(vector<string> leader_board);
 void clear_screen();
   
 #endif

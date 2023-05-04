0cb4304c70f5e35c163ac3cb7ddbb2db0feeac77
Muhammad Abdur Rahman Saad <63783742+Alpha-30-creator@users.noreply.github.com>
2023-05-05 02:51:51 +0800

    debug get leaderboard

--- fileManipulation.cpp
+++ fileManipulation.cpp
@@ -152,6 +152,8 @@ vector<string> get_leaderboard(){
 
     //check if file exists
     if (fin.fail()) {
+        //close file so we can open in write mode
+        fin.close()
         //if does not exist, create the file
         ofstream fout;
         fout.open("leaderboard.txt");
